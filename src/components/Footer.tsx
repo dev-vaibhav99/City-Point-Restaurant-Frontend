1912e8d30267d7328e790238cf73d5dbbb1972b8
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-06 01:36:55 +0530

    Add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="flex w-full h-10 bottom-0 fixed bg-gray-50 justify-between items-center">
+      <div className="flex items-center">
+        <strong className="p-2">Copyright @ Career Infotech 2024</strong>{" "}
+        <p>All Rights Reserved</p>
+      </div>
+      <div className="flex items-center">
+        <strong>Version</strong>
+        <p className="p-2">1.0.0</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
