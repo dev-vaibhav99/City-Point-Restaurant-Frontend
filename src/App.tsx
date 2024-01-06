ddfaab10e2e48561fa7ce911de281f462dedbb7d
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-06 12:58:44 +0530

    Render Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <>
       <Header />
+      <Menu />
       <Footer />
     </>
   );
