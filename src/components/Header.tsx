24091e9690f5fb7faede94edd90b1583beb9eecd
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-06 13:32:59 +0530

    Implement React Icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,16 @@
-const Header = () => {
+import React from "react";
+import { HiMiniBars3 } from "react-icons/hi2";
+import { HiOutlinePower } from "react-icons/hi2";
+const Header: React.FC = () => {
   return (
-    <header className="h-14 top-0 left-0 flex justify-between items-center text-xl transition-transform md:ml-60 ">
+    <header className="h-14 top-0 left-0 flex justify-between items-center text-lg transition-transform md:ml-60 ">
       <div className="h-full bg-gray-100 w-full flex justify-between items-center px-5 text-gray-700">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 hover:text-gray-400 duration-300 hover:cursor-pointer"
+        <HiMiniBars3 className="text-gray-700 hover:text-gray-400 duration-300" />
+        <div
+          className="flex gap-2 items-center hover:text-gray-400 duration-300 hover:cursor-pointer"
+          onClick={() => alert("Logged out")}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-        <div className="flex gap-2 items-center hover:text-gray-400 duration-300 hover:cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9"
-            />
-          </svg>
-
+          <HiOutlinePower />
           <p>Logout</p>
         </div>
       </div>
